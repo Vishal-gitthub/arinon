dc62ef259c667ead06489eff072d4f569dd3c48d
Vishal Sharma <vssv8808@gmail.com>
2024-11-29 11:49:15 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import { Routes, Route } from "react-router-dom";
 import DigitalArinonPage from "./Digital_Arinon/DigitalArinonPage";
 import Game_page from "./Games_Arinon/Game_page";
 import "./SecondaryStyle.css"
-import TestPage from "./Pages/TestPage";
 const App = () => {
   return (
     <div>
       <CustomCursor />
       <Routes>
         <Route path="/" element={<FrontPage />} />
-        <Route path="/test" element={<TestPage />} />
         <Route path="/arinon-digital" element={<DigitalArinonPage />} />
         <Route path="/arinon-games" element={<Game_page />} />
 
